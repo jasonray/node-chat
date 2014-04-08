26c36c0ec0c108f2107d935bc02ed1c2cd2b7e01
Jason Ray <jason.ray@agilex.com>
2014-04-08 19:16:38 -0400

    output received data to all listeners

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var net  = require("net");
 var chatServer = net.createServer();
+var clientList = [];
 
 chatServer.on('connection', function(client) {
 	client.write('Hi!\n');
 
+	clientList.push(client);
+
 	client.on('data', function(data) {
-		console.log(data);		
+		for (var i=0;i<clientList.length;i++) {
+			clientList[i].write(data);
+		}
 	})
 })
 
